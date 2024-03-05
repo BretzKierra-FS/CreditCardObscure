c83a74f4e6c7bfb64a3aff20bbe45b865cd9dedb
BretzKierra-FS <122029593+BretzKierra-FS@users.noreply.github.com>
2024-03-05 16:25:05 -0500

    Update obscureCreditCard.js

--- obscureCreditCard.js
+++ obscureCreditCard.js
@@ -3,13 +3,13 @@ const obscureCreditCard = (cardNumber) => {
   if (
     typeof cardNumber !== 'string' ||
     cardNumber.length < 12 ||
-    cardNumber.length > 16 // <-- Corrected comparison
+    cardNumber.length > 16 
   ) {
     return 'Invalid Credit Card';
   }
 
   // Take the last 4 digits of the card number
-  const lastFour = cardNumber.slice(-4); // <-- Corrected method
+  const lastFour = cardNumber.slice(-4); 
 
   // Replace the rest of the digits with '*'
   const sensorCard = '*'.repeat(cardNumber.length - 4);
